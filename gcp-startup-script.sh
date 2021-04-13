1fcc7de79bf46afe52a6e8dc3068dbb47b928302
Jorge Cortes <jorge.cortes@microsoft.com>
2021-04-13 16:51:00 -0500

    Single line

--- gcp-startup-script.sh
+++ gcp-startup-script.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-apt update -y && sudo apt upgrade -y
-apt install -y tcpdump
-apt install -y hping3
-apt install -y inetutils-traceroute
-apt install -y netcat
+apt update -y && sudo apt upgrade -y && sudo apt install tcpdump hping3 inetutils-traceroute netcat
